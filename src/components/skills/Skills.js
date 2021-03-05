b51d2872f496b2d63ae4edc4400cab7f5eb3005a
uriisan23 <63766620+uriisan23@users.noreply.github.com>
2021-03-05 12:06:58 -0600

    Update Skills.js

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -35,10 +35,10 @@ const skills = (props) => {
    
    <div className="navMenu">
      <Switch>
-         <Route exact path="/lenguajes"  component={Lenguajes}></Route>
-         <Route exact path="/frameworks" component={Frames}></Route>
-         <Route exact path="/herramientas" component={Herramientas}></Route>
-         <Route exact path="/otros" component={Otros}></Route>
+         <Route  path="/lenguajes"  component={Lenguajes}></Route>
+         <Route  path="/frameworks" component={Frames}></Route>
+         <Route  path="/herramientas" component={Herramientas}></Route>
+         <Route  path="/otros" component={Otros}></Route>
          <Route component={Lenguajes}></Route>
      </Switch>
     
