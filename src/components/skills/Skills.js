32c87f10ab5f05054a80dfe25c603247763f4e30
Uriel Sanchez Lopez <usanchez82@outlook.com>
2021-03-05 12:12:18 -0600

    Rewrite RouteBrowser

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -24,10 +24,10 @@ const skills = (props) => {
   <div className="navMenu"  id="skills">
        <nav>
            <ul>
-               <li className="List"><NavLink to="./lenguajes">LENGUAJES </NavLink></li>
-               <li className="List"><NavLink to="./frameworks" >FRAMEWORKS</NavLink></li>
-               <li className="List"><NavLink to="./herramientas">HERRAMIENTAS</NavLink></li>
-               <li className="List"><NavLink to="./otros">OTROS</NavLink></li>
+               <li className="List"><NavLink to="/lenguajes">LENGUAJES </NavLink></li>
+               <li className="List"><NavLink to="/frameworks" >FRAMEWORKS</NavLink></li>
+               <li className="List"><NavLink to="/herramientas">HERRAMIENTAS</NavLink></li>
+               <li className="List"><NavLink to="/otros">OTROS</NavLink></li>
            </ul>
        </nav>
 
@@ -35,6 +35,7 @@ const skills = (props) => {
    
    <div className="navMenu">
      <Switch>
+        
          <Route exact path="/lenguajes"  component={Lenguajes}></Route>
          <Route exact path="/frameworks" component={Frames}></Route>
          <Route path="/herramientas" component={Herramientas}></Route>
